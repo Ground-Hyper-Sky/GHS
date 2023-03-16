af215395ada4d6f36cc5a3e9bf74de7e65f5e638
qing1512 <x15229717260@163.com>
2023-03-16 23:25:54 +0800

    删除无用的common.js,添加两张3D皮肤图片（待更换）

--- js/index.js
+++ js/index.js
@@ -1,84 +1,74 @@
-	// 展示图片滚动
-	let smoo = document.querySelector('.zhan_gundong');
-	let smoo_ = document.querySelector('.zhan_gundong1');
+	// // 展示图片滚动
+	// let smoo = document.querySelector('.zhan_gundong');
+	// let smoo_ = document.querySelector('.zhan_gundong1');
 	
-	let left = 0;
-	let left_ = 0;
+	// let left = 0;
+	// let left_ = 0;
 	
-	let stop;
-	let stop_;
+	// let stop;
+	// let stop_;
 	
-	smoo.innerHTML += smoo.innerHTML;
-	smoo_.innerHTML += smoo_.innerHTML;
+	// smoo.innerHTML += smoo.innerHTML;
+	// smoo_.innerHTML += smoo_.innerHTML;
 	
 	 // 机器图片滚动
-	 let whith = document.getElementsByClassName('whith').offsetWidth;
-	// let lil = document.querySelector('.zhan_gundong');
-	// let lll = lil.children[0]
-	// let wid = null;
-		 // setInterval(function () {
+	//  let whith = document.getElementsByClassName('whith').offsetWidth;
+	// // let lil = document.querySelector('.zhan_gundong');
+	// // let lll = lil.children[0]
+	// // let wid = null;
+	// 	 // setInterval(function () {
 
-		 // },1000);
-		stop = setInterval(function(){
-			let wid = lll.clientWid;
-			console.log(wid);
-		left -= 3
-		if (left === -(4 * wid)){
-			left = 0;
-		};
-		smoo.style.left = left + 'px';
-	 },10);
-	 smoo.onmouseover = function() {
-	 		clearInterval(stop);
-	 }
-	 smoo.onmouseout = function() {
-		 stop =  setInterval(function(){
-		 		left -= 10
-		 		if (left === -(4 * wid)) {
-		 			left = 0;
-		 		}
-		 		smoo.style.left = left + 'px';
-		 },10);
-	 }
+	// 	 // },1000);
+	// 	stop = setInterval(function(){
+	// 		let wid = lll.clientWid;
+	// 		console.log(wid);
+	// 	left -= 3
+	// 	if (left === -(4 * wid)){
+	// 		left = 0;
+	// 	};
+	// 	smoo.style.left = left + 'px';
+	//  },10);
+	//  smoo.onmouseover = function() {
+	//  		clearInterval(stop);
+	//  }
+	//  smoo.onmouseout = function() {
+	// 	 stop =  setInterval(function(){
+	// 	 		left -= 10
+	// 	 		if (left === -(4 * wid)) {
+	// 	 			left = 0;
+	// 	 		}
+	// 	 		smoo.style.left = left + 'px';
+	// 	 },10);
+	//  }
 	 
-	 // 建筑图片滚动
-	 	stop_ = setInterval(function(){
-	 	left_ -= 3
-	 	if (left_ === -(4 * 900)) {
-	 		left_ = 0;
-	 	}
-	 	smoo_.style.left = left_ + 'px';
-	  },10);
-	 smoo_.onmouseover = function() {
-	 	 	clearInterval(stop_);
-	 }
-	 smoo_.onmouseout = function() {
-	 	stop_ =  setInterval(function(){
-	 	left_ -= 3
-	 	if (left_ === -(4 * 900)) {
-	 		left_ = 0;
-	 	}
-	 	smoo_.style.left = left_ + 'px';
-	  },10);}
-	  
-	  // 点击英语弹出敬请期待
-	  let ick = document.querySelector('.click');//英语一
-	  let ick_ = document.querySelector('.click_');//英语二
-	  //下拉框
-	  let oU_ = document.getElementsByClassName('.la_la_');//下拉框二
-	  ick.onclick = function () {
-	  	alert("论坛我都咕咕咕了,你还想要英文网页?");
-	  }
-	  ick_.onclick = function () {
-	  	alert("论坛我都咕咕咕了,你还想要英文网页?");
-	  }
-	  // -----------------------------------------------------------------------------------------------------------------
-	  //点击论坛弹出敬请期待
-	  let forum = document.querySelector('.forum');
-	  let forum_ = document.querySelector('.forum_');
-	  forum.onclick = function () {
-	  	alert("是的,我是鸽子,我咕咕咕了;什么?敬请期待,不存在的。");
-	  };
-	  forum_.onclick = function () {
-	  	alert("是的,我是鸽子,我咕咕咕了;什么?敬请期待,不存在的。");
-	  };
+	//  // 建筑图片滚动
+	//  	stop_ = setInterval(function(){
+	//  	left_ -= 3
+	//  	if (left_ === -(4 * 900)) {
+	//  		left_ = 0;
+	//  	}
+	//  	smoo_.style.left = left_ + 'px';
+	//   },10);
+	//  smoo_.onmouseover = function() {
+	//  	 	clearInterval(stop_);
+	//  }
+	//  smoo_.onmouseout = function() {
+	//  	stop_ =  setInterval(function(){
+	//  	left_ -= 3
+	//  	if (left_ === -(4 * 900)) {
+	//  		left_ = 0;
+	//  	}
+	//  	smoo_.style.left = left_ + 'px';
+	//   },10);}
+	
+	// 点击英语弹出敬请期待
+	let ick = document.querySelector('.click');//英语一
+	ick.onclick = function () {
+		alert("敬请期待昂！");
+	}
+	// -----------------------------------------------------------------------------------------------------------------
+	//点击论坛弹出敬请期待
+	let forum = document.querySelector('.forum');
+	forum.onclick = function () {
+		alert("是的,我是鸽子,我咕咕咕了;什么?敬请期待,不存在的。");
+	};
