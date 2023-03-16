fc49aacbcfd4c9f693fdf78f9592abc1d689c418
qing1512 <x15229717260@163.com>
2023-03-16 15:46:01 +0800

    第一次提交，无修改

---
+++ js/common.js
@@ -0,0 +1,89 @@
+// 	// 下拉
+// 	let op = document.querySelector('.la');
+// 	let oUl = document.querySelector('.la_la');
+// 	// la_lonload = function () {
+// 		op.addEventListener('mouseover', function() {
+// 			oUl.style.display = 'inline-block';
+// 		})
+// 		let outtime;
+// 		op.addEventListener('mouseout', function() {
+// 			clearTimeout(outtime);
+// 			outtime = setTimeout(function(){
+// 				oUl.style.display = 'none';
+// 			},1000)
+// 		})	
+// 		// op.addEventListener('mouseout', function() {
+// 		// 	outtime;
+// 		// })
+// 		oUl.addEventListener('mouseover',function(){
+// 			clearTimeout(outtime);
+// 		})
+// 		oUl.addEventListener('mouseout',function(){
+// 			oUl.style.display = 'none';
+// 			// outtime;	
+// 		})
+// 		// ls = setInterval(function(){
+// 		// 	console.log(window.pageYOffset)
+// 		// },100)
+// 	// }
+//   // ----------------------------------------------------------------------------------------------------------------
+  
+
+// // -----------------------------------------------------------------------------------------------------------------
+
+// // -----------------------------------------------------------------------------------------------------------------
+// 	// 展示图片滚动
+// 	// let smoo = document.querySelector('.zhan_gundong');
+// 	// let smoo_ = document.querySelector('.zhan_gundong1');
+// 	// let lll = smoo.children[0]
+// 	// let wid = null;
+// 	// let left = 0;
+// 	// let left_ = 0;
+	
+// 	// let stop;
+// 	// let stop_;
+// 	// smoo.innerHTML += smoo.innerHTML;
+// 	// smoo_.innerHTML += smoo_.innerHTML;
+// 	//  // 机器图片滚动
+// 	// 	stop = setInterval(function(){
+
+// 	// 	left -= 3
+// 	// 	if (left === -(4 * 900)) {
+// 	// 		left = 0;
+// 	// 	}
+// 	// 	smoo.style.left = left + 'px';
+// 	//  },10);
+	 
+// 	//  smoo.onmouseover = function() {
+// 	//  		clearInterval(stop);
+// 	//  }
+// 	//  smoo.onmouseout = function() {
+// 	// 	 stop =  setInterval(function(){
+// 	// 	 		left -= 3
+// 	// 	 		if (left === -(4 * 900)) {
+// 	// 	 			left = 0;
+// 	// 	 		}
+// 	// 	 		smoo.style.left = left + 'px';
+// 	// 	 },10);
+// 	//  }
+	 
+// 	//  // 建筑图片滚动
+// 	//  	stop_ = setInterval(function(){
+// 	//  	left_ -= 3
+// 	//  	if (left_ === -(4 * 900)) {
+// 	//  		left_ = 0;
+// 	//  	}
+// 	//  	smoo_.style.left = left_ + 'px';
+// 	//   },10);
+// 	//  smoo_.onmouseover = function() {
+// 	//  	 	clearInterval(stop_);
+// 	//  }
+// 	//  smoo_.onmouseout = function() {
+// 	//  	stop_ =  setInterval(function(){
+// 	//  	left_ -= 3
+// 	//  	if (left_ === -(4 * 900)) {
+// 	//  		left_ = 0;
+// 	//  	}
+// 	//  	smoo_.style.left = left_ + 'px';
+// 	//   },10);}
+// 	// 成员旋转木马轮播图
